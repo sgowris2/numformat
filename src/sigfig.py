54def0358cd27cefeadbd61f4a836f5e3d966d4b
sgowris2 <sgowris2@gmail.com>
2021-03-27 15:59:01 +0530

    Added max_sig_figs implementation and tests

--- src/sigfig.py
+++ src/sigfig.py
@@ -1,38 +1,90 @@
 from decimal import Decimal
+from math import floor, log10
 
 
 class SigFig:
 
-    def to_precision(self, value, precision=None):
-        if value == 0:
-            return 0
-        n = self._SigFig__get_sigfigs(value)
+    @staticmethod
+    def sfformat(value, max_sig_figs: int = None,
+                 max_str_length: int = None,
+                 allow_scientific_notation: bool = True):
+        SigFig.__check_sfformat_args(max_sig_figs=max_sig_figs,
+                                     max_str_length=max_str_length,
+                                     allow_scientific_notation=allow_scientific_notation)
+
+        value = SigFig._SigFig__convert_to_numeric(value)
+
+        sig_figs = SigFig._SigFig__get_sigfigs(value)
+
+        value_str = SigFig.__convert_to_desired_sigfigs_str(max_sig_figs, sig_figs, value)
+
+        if max_str_length is not None:
+            if len(value_str) <= max_str_length:
+                return value_str
+
+        return value_str
 
     # Private Methods
 
     @staticmethod
-    def __convert_to_float(value):
-        if type(value) in (int, float):
-            return float(value)
-        raise ValueError('Could not load input - {}'.format(value))
+    def __convert_to_desired_sigfigs_str(max_sig_figs, sig_figs, value):
+        if max_sig_figs is not None:
+            if max_sig_figs >= sig_figs:
+                return str(value)
+            else:
+                order = SigFig.__get_order(value)
+                value = round(value, max_sig_figs - order)
+                if (value % 1) == 0:
+                    value = int(value)
+                return str(value)
+        return str(value)
 
     @staticmethod
-    def __get_sigfigs(value, strict=False):
+    def __get_sigfigs(value):
 
         if isinstance(value, int):
-            return SigFig.__get_sigfigs_for_integer_string(value)
-        if isinstance(value, float):
-            return SigFig.__get_sigfigs_for_decimal_string(value)
-        if isinstance(value, str):
-            value = value.strip().strip('-')
-            if value.isdigit():
-                value = int(value)
-                return SigFig.__get_sigfigs_for_integer_string(value)
-            else:
-                value = SigFig.__to_decimal(value)
-            return SigFig.__get_sigfigs_for_decimal_string(value)
+            return SigFig.__get_sigfigs_for_integer(value)
+        if isinstance(value, Decimal):
+            return SigFig.__get_sigfigs_for_decimal(value)
 
         raise ValueError('Could not find sigfigs for input - {}'.format(value))
+
+    @staticmethod
+    def __get_order(value):
+        if abs(float(value) - 0.0) < 0.000000001:
+            return 0
+        order = floor(log10(abs(value))) + 1
+        return order
+
+    @staticmethod
+    def __convert_to_numeric(value):
+        if isinstance(value, int):
+            return value
+        elif isinstance(value, float):
+            value = SigFig.__to_decimal(repr(value))
+            return value
+        elif isinstance(value, str):
+            digits_only_value = value.strip().strip('-')
+            if digits_only_value.isdigit():
+                value = int(value)
+            else:
+                value = SigFig.__to_decimal(value)
+            return value
+        else:
+            raise ValueError('Input was not in any of the supported formats '
+                             'int, float, decimal.Decimal. Received object of type {}'.format(type(value)))
+
+    @staticmethod
+    def __check_sfformat_args(max_sig_figs: int = None,
+                              max_str_length: int = None,
+                              allow_scientific_notation: bool = True):
+        if max_sig_figs is not None and max_sig_figs < 1:
+            raise ValueError('\'max_sig_figs\' must be >= 1. Received {}'.format(max_sig_figs))
+        if max_str_length is not None and max_str_length < 3:
+            raise ValueError('\'max_str_length\' must be >= 3. Received {}'.format(max_sig_figs))
+        if not isinstance(allow_scientific_notation, bool):
+            raise ValueError(
+                '\'allow_scientific_notation\' must be a boolean. Received {}'.format(allow_scientific_notation))
 
     @staticmethod
     def __to_decimal(value):
@@ -43,11 +95,11 @@ class SigFig:
         return value
 
     @staticmethod
-    def __get_sigfigs_for_integer_string(value):
+    def __get_sigfigs_for_integer(value):
         return len(str(abs(value)).strip('0'))
 
     @staticmethod
-    def __get_sigfigs_for_decimal_string(value):
+    def __get_sigfigs_for_decimal(value):
         value_str = str(abs(value))
         decimal_point_index = len(value_str.split('.')[0])
         for i in range(len(value_str)):
---
+++ test/test_sfformat.py
@@ -0,0 +1,129 @@
+import unittest
+
+from sigfig import SigFig
+
+
+class TestSfformat(unittest.TestCase):
+
+    def test_zeros(self):
+        self.assertEqual('0', SigFig.sfformat(0, max_sig_figs=2))
+        self.assertEqual('0.0', SigFig.sfformat(0.0, max_sig_figs=2))
+        self.assertEqual('0.0', SigFig.sfformat(0.000, max_sig_figs=2))
+        self.assertEqual('0.0', SigFig.sfformat(0.000, max_sig_figs=4))
+        self.assertEqual('0.000', SigFig.sfformat('0.000', max_sig_figs=2))
+        self.assertEqual('0.000', SigFig.sfformat('0.000', max_sig_figs=4))
+
+    def test_integers_max_sig_figs_greater_than_actual_sig_figs(self):
+        # Positive integers
+        self.assertEqual('1', SigFig.sfformat(1, max_sig_figs=2))
+        self.assertEqual('2', SigFig.sfformat(2, max_sig_figs=2))
+        self.assertEqual('13', SigFig.sfformat(13, max_sig_figs=3))
+        self.assertEqual('1000', SigFig.sfformat(1000, max_sig_figs=2))
+        self.assertEqual('459284756', SigFig.sfformat(459284756, max_sig_figs=18))
+
+        # Negative integers
+        self.assertEqual('-1', SigFig.sfformat(-1, max_sig_figs=2))
+        self.assertEqual('-2', SigFig.sfformat(-2, max_sig_figs=2))
+        self.assertEqual('-13', SigFig.sfformat(-13, max_sig_figs=3))
+        self.assertEqual('-1000', SigFig.sfformat(-1000, max_sig_figs=2))
+        self.assertEqual('-459284756', SigFig.sfformat(-459284756, max_sig_figs=18))
+
+    def test_integers_max_sig_figs_less_than_actual_sig_figs(self):
+        # Positive integers
+        self.assertEqual('10', SigFig.sfformat(11, max_sig_figs=1))
+        self.assertEqual('30', SigFig.sfformat(29, max_sig_figs=1))
+        self.assertEqual('140', SigFig.sfformat(137, max_sig_figs=2))
+        self.assertEqual('1000', SigFig.sfformat(1001, max_sig_figs=2))
+        self.assertEqual('459000000', SigFig.sfformat(459284756, max_sig_figs=3))
+
+        # Negative integers
+        self.assertEqual('-10', SigFig.sfformat(-11, max_sig_figs=1))
+        self.assertEqual('-30', SigFig.sfformat(-29, max_sig_figs=1))
+        self.assertEqual('-140', SigFig.sfformat(-137, max_sig_figs=2))
+        self.assertEqual('-1000', SigFig.sfformat(-1001, max_sig_figs=2))
+        self.assertEqual('-459000000', SigFig.sfformat(-459284756, max_sig_figs=3))
+
+    def test_floats_max_sig_figs_greater_than_actual_sig_figs(self):
+        # Positive floats
+        self.assertEqual('0.0032', SigFig.sfformat(0.0032, max_sig_figs=3))
+        self.assertEqual('145.89', SigFig.sfformat(145.89, max_sig_figs=6))
+        self.assertEqual('1300001.0', SigFig.sfformat(1300001.0, max_sig_figs=10))
+        self.assertEqual('12700', SigFig.sfformat(12700.00, max_sig_figs=4))
+        self.assertEqual('12700.0', SigFig.sfformat(12700.00, max_sig_figs=8))
+
+        # Negative floats
+        self.assertEqual('-0.0032', SigFig.sfformat(-0.0032, max_sig_figs=3))
+        self.assertEqual('-145.89', SigFig.sfformat(-145.89, max_sig_figs=6))
+        self.assertEqual('-1300001.0', SigFig.sfformat(-1300001.0, max_sig_figs=10))
+        self.assertEqual('-12700', SigFig.sfformat(-12700.00, max_sig_figs=4))
+        self.assertEqual('-12700.0', SigFig.sfformat(-12700.00, max_sig_figs=8))
+
+    def test_floats_max_sig_figs_less_than_actual_sig_figs(self):
+        # Positive floats
+        self.assertEqual('0.003', SigFig.sfformat(0.0032, max_sig_figs=1))
+        self.assertEqual('146', SigFig.sfformat(145.89, max_sig_figs=3))
+        self.assertEqual('1300000', SigFig.sfformat(1300001.0, max_sig_figs=3))
+        self.assertEqual('13000', SigFig.sfformat(12700.00, max_sig_figs=2))
+
+        # Negative floats
+        self.assertEqual('-0.003', SigFig.sfformat(-0.0032, max_sig_figs=1))
+        self.assertEqual('-146', SigFig.sfformat(-145.89, max_sig_figs=3))
+        self.assertEqual('-1300000', SigFig.sfformat(-1300001.0, max_sig_figs=3))
+        self.assertEqual('-13000', SigFig.sfformat(-12700.00, max_sig_figs=2))
+
+    def test_strings_max_sig_figs_greater_than_actual_sig_figs(self):
+        # Positive integers
+        self.assertEqual('1', SigFig.sfformat('1', max_sig_figs=2))
+        self.assertEqual('2', SigFig.sfformat('2', max_sig_figs=2))
+        self.assertEqual('13', SigFig.sfformat('13', max_sig_figs=3))
+        self.assertEqual('1000', SigFig.sfformat('1000', max_sig_figs=2))
+        self.assertEqual('459284756', SigFig.sfformat('459284756', max_sig_figs=18))
+
+        # Negative integers
+        self.assertEqual('-1', SigFig.sfformat('-1', max_sig_figs=2))
+        self.assertEqual('-2', SigFig.sfformat('-2', max_sig_figs=2))
+        self.assertEqual('-13', SigFig.sfformat('-13', max_sig_figs=3))
+        self.assertEqual('-1000', SigFig.sfformat('-1000', max_sig_figs=2))
+        self.assertEqual('-459284756', SigFig.sfformat('-459284756', max_sig_figs=18))
+
+        # Positive floats
+        self.assertEqual('0.0032', SigFig.sfformat('0.0032', max_sig_figs=3))
+        self.assertEqual('145.89', SigFig.sfformat('145.89', max_sig_figs=6))
+        self.assertEqual('1300001.0', SigFig.sfformat('1300001.0', max_sig_figs=10))
+        self.assertEqual('12700', SigFig.sfformat('12700.00', max_sig_figs=4))
+        self.assertEqual('12700.00', SigFig.sfformat('12700.00', max_sig_figs=8))
+
+        # Negative floats
+        self.assertEqual('-0.0032', SigFig.sfformat('-0.0032', max_sig_figs=3))
+        self.assertEqual('-145.89', SigFig.sfformat('-145.89', max_sig_figs=6))
+        self.assertEqual('-1300001.0', SigFig.sfformat('-1300001.0', max_sig_figs=10))
+        self.assertEqual('-12700', SigFig.sfformat('-12700.00', max_sig_figs=4))
+        self.assertEqual('-12700.00', SigFig.sfformat('-12700.00', max_sig_figs=8))
+    
+    def test_strings_max_sig_figs_less_than_actual_sig_figs(self):
+
+        # Positive integers
+        self.assertEqual('10', SigFig.sfformat('11', max_sig_figs=1))
+        self.assertEqual('30', SigFig.sfformat('29', max_sig_figs=1))
+        self.assertEqual('140', SigFig.sfformat('137', max_sig_figs=2))
+        self.assertEqual('1000', SigFig.sfformat('1001', max_sig_figs=2))
+        self.assertEqual('459000000', SigFig.sfformat('459284756', max_sig_figs=3))
+
+        # Negative integers
+        self.assertEqual('-10', SigFig.sfformat('-11', max_sig_figs=1))
+        self.assertEqual('-30', SigFig.sfformat('-29', max_sig_figs=1))
+        self.assertEqual('-140', SigFig.sfformat('-137', max_sig_figs=2))
+        self.assertEqual('-1000', SigFig.sfformat('-1001', max_sig_figs=2))
+        self.assertEqual('-459000000', SigFig.sfformat('-459284756', max_sig_figs=3))
+
+        # Positive floats
+        self.assertEqual('0.003', SigFig.sfformat('0.0032', max_sig_figs=1))
+        self.assertEqual('146', SigFig.sfformat('145.89', max_sig_figs=3))
+        self.assertEqual('1300000', SigFig.sfformat('1300001.0', max_sig_figs=3))
+        self.assertEqual('13000', SigFig.sfformat('12700.00', max_sig_figs=2))
+
+        # Negative floats
+        self.assertEqual('-0.003', SigFig.sfformat('-0.0032', max_sig_figs=1))
+        self.assertEqual('-146', SigFig.sfformat('-145.89', max_sig_figs=3))
+        self.assertEqual('-1300000', SigFig.sfformat('-1300001.0', max_sig_figs=3))
+        self.assertEqual('-13000', SigFig.sfformat('-12700.00', max_sig_figs=2))
